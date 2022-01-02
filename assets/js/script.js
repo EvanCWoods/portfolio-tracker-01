52137c6e747ed4f3baf96e60f9d8322437fc5279
Evan Woods <you@example.com>
2022-01-02 17:52:43 +1100

    added logic to handle form submission and created a new function to get items from local storage to be displayed on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,39 @@ let addPositionButton = document.getElementById("add-position-button");
 let newPositionFormSection = document.getElementById("new-position-form-section");
 let submitPositionButton = document.getElementById("submit-new-position-button");
 
+let newPositionDetailsList = [];
+let getPositionDetailsList = [];
 
 // Function to handle the form for new position entries
-let getPositionDetails = () => {
+let setPositionDetails = () => {
     submitPositionButton.addEventListener("click", (event) => {
         event.preventDefault();
-
-        newPositionFormSection.style.display = "none";  // Set the display of the form back to none on submit
+       
+        let positionTitle = document.getElementById("position-name");
+        let positionUnits = document.getElementById("position-units");
+        let unitValue = document.getElementById("unit-value");
+        let totalValue = positionUnits.value * unitValue.value;
+       
+        let newPosition = ({
+            title: positionTitle.value,
+            units: Number(positionUnits.value),
+            valuePerUnit: Number(unitValue.value),
+            totalValue: totalValue
+        });
+        localStorage.setItem(`${positionTitle.value}`, JSON.stringify(newPosition));
+        positionTitle.value = "";
+        positionUnits.value = "";
+        unitValue.value = "";
+        console.log(newPositionDetailsList);
     });
+}
+
+// Function to handle the gettign of data from local storage
+let getPositionDetails = () => {
+    for (let i=0; i<localStorage.length; i++) {
+        positionInfo = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        getPositionDetailsList.push(positionInfo);
+    }
 }
 
 
@@ -19,6 +44,7 @@ let main = () => {
     addPositionButton.addEventListener("click", () => {
         newPositionFormSection.style.display = "block";
     });
+    setPositionDetails();
     getPositionDetails();
 }
 
